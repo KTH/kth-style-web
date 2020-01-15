db063d57c51f92314484aee3b3e13ea9386af28d
Jonolov <jon.stjarnstrom@gmail.com>
2020-01-15 14:05:19 +0100

    Removed the tables.

--- public/js/app/pages/TypographyPage.jsx
+++ public/js/app/pages/TypographyPage.jsx
@@ -3,73 +3,88 @@ import { inject, observer } from 'mobx-react'
 
 import NavBar from '../components/NavBar'
 
-const TableRows = ({ list }) => {
+const TypographyItems = ({ list }) => {
   return list.map((row, index) => (
-    <tr key={index} className="tableRow">
-      <td>
-        <span className={row.className}>{row.text}</span>
-      </td>
-      <td>
+    <div key={index} className="itemBox typographyItem border">
+      <span className={row.className}>{row.text}</span>
+      <p>
+        <strong>HTML: </strong>
         <code className="language-html">{row.html}</code>
-      </td>
-      <td>
-        <code>{row.css}</code>
-      </td>
-      <td>
+      </p>
+      <p>
+        <strong>Sass: </strong>
+        <code>{row.sass}</code>
+      </p>
+      <div>
+        <p>
+          <strong>Användningsområde: </strong>
+        </p>
         <ul>
           {row.usedFor.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul>
-      </td>
-    </tr>
+      </div>
+    </div>
   ))
 }
 
 export const Main = props => {
   const tableRows = [
-    { className: 't1', text: 'H1 - Heading', html: '<h1></h1>', css: '@include t1;', usedFor: ['Rubrik nivå 1'] },
-    { className: 't2', text: 'H2 - Heading', html: '<h2></h2>', css: '@include t2;', usedFor: ['Rubrik nivå 2'] },
+    {
+      className: 't1',
+      text: 'H1 - Heading',
+      html: '<h1></h1>',
+      sass: 'h1 { @include t1; }',
+      usedFor: ['Rubrik nivå 1']
+    },
+    {
+      className: 't2',
+      text: 'H2 - Heading',
+      html: '<h2></h2>',
+      sass: 'h2 { @include t2; }',
+      usedFor: ['Rubrik nivå 2']
+    },
     {
       className: 't24',
       text: 'H2 - Heading',
       html: '<h2 class="t24"></h2>',
-      css: '@include t24;',
+      sass: 'h2 { @include t24; }',
       usedFor: ['Rubrik puffblock bred']
     },
     {
       className: 't3',
       text: 'H3 - Heading',
       html: '<h3></h3>',
-      css: '@include t3;',
+      sass: 'h3 { @include t3; }',
       usedFor: ['Rubrik nivå 3 (i artikel)']
     },
     {
       className: 't4',
       text: 'H4 - Heading',
       html: '<h4></h4>',
-      css: '@include t4;',
+      sass: 'h4 { @include t4; }',
       usedFor: ['Rubrik nivå 4', 'Blockrubrik', 'Rubrik puffblock smal', 'Menyrubrik', 'Sitename']
     },
     {
       className: 't11',
       text: 'P - Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       html: '<p></p>',
-      css: '@include t11;',
+      sass: '@include t11;',
       usedFor: ['Brödtext puff']
     },
     {
       className: 't12',
       text: 'P - Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       html: '<p class="t12"></p>',
-      css: '@include t12;',
+      sass: '@include t12;',
       usedFor: ['Brödtext artikel']
     },
     {
       className: 't10',
       text: 'P - Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       html: '<p class="t10"></p>',
-      css: '@include t10;',
+      sass: '@include t10;',
       usedFor: ['Ingress', 'Megameny', 'Blockquote']
     }
   ]
@@ -85,17 +100,11 @@ export const Main = props => {
         användas istället för klassnamnen nedan. Inget klassnamn eller mixin krävs för standard-element av typen
         h1,h2,h3,h4 och p utan endast för vissa varianter av dessa.
       </p>
-      <table className="table">
-        <thead className="thead-default">
-          <tr>
-            <th>Exempeltext</th>
-            <th>Html</th>
-            <th>Sass mixin</th>
-            <th>Användningsområde</th>
-          </tr>
-          <TableRows list={tableRows} />
-        </thead>
-      </table>
+
+      <div className="itemContainer">
+        <TypographyItems list={tableRows} />
+      </div>
+
       {/* <p>{`Message from styleStore: ${props.message}`}</p> */}
     </div>
   )
