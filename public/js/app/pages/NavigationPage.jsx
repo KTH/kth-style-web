473e2b7efcb46bb79b189b20b84a72c457f8d6b8
Niklas Olsson <hoyce@kth.se>
2020-08-27 16:22:11 +0200

    Added info on navigatopn page

--- public/js/app/pages/NavigationPage.jsx
+++ public/js/app/pages/NavigationPage.jsx
@@ -2,12 +2,71 @@ import React, { Component, Fragment } from 'react'
 import { inject, observer } from 'mobx-react'
 
 import NavBar from '../components/NavBar'
+import CodeSnippet from '../components/CodeSnippet'
 
 export const Main = props => {
   return (
     <div className="mainContent">
       <h1>Navigation</h1>
-      <p>Navigation så som menyer ... </p>
+      <p>Navigation is based on several menus in different contexts. Below you will find examples of these menus. </p>
+      <h2>Main navigation</h2>
+      <p>
+        The main navigation is placed on the left side of the main contnent. Real life examples can be found on KTH.se,
+        for examle here: <a href="https://www.kth.se/en/aktuellt/">Aktuellt</a>
+      </p>
+      <p>A shortened example of the code can be seen below:</p>
+      <CodeSnippet
+        snippet={`<nav id="mainMenu" class="col navbar navbar-expand-lg navbar-light">
+  <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="nav">
+        <li class="parentLink"><a href="/">KTH</a></li>
+    </ul>
+    <ul class="nav nav-ancestor">
+      <li><span class="nav-item ancestor">Aktuellt</span></li>
+      </ul>
+      <ul class="nav nav-list">
+        <li class="nav-item leaf"><a class="nav-link" href="/aktuellt/nyheter">Nyheter</a></li>
+        <li class="nav-item node"><a class="nav-link" href="/aktuellt/nyhetsteman">Aktuella nyhetsteman</a></li>
+        <li class="nav-item node"><a class="nav-link" href="/aktuellt/kalender">KTH-kalendern</a></li>
+      </ul>
+    </div>
+  </nav>`}
+      />
+
+      <h2>Secondary menu</h2>
+      <p>
+        The secondary menu is placed in the top right corner of the header and contains entrancies for students,
+        alumnies, employees and a language switcher. This menu is more or less included on all pages of www.kth.se.
+      </p>
+      <p>
+        This menu is preferably imported to the application you are building via{' '}
+        <a href="https://github.com/KTH/kth-node-cortina-block" target="_blank" className="external-link">
+          kth-node-cortina-block
+        </a>{' '}
+        or similar functionality to keep the navigation in the header consistent throughout the sites.
+      </p>
+      <CodeSnippet
+        snippet={`<nav aria-label="global secondary">
+  <ul>
+    <li><a href="/student">Student</a>
+    </li><li><a href="/alumni">Alumn</a>
+    </li><li><a href="https://intra.kth.se/">Anställd</a>
+    </li><li><a href="https://www.kth.se/en" hreflang="en-UK">International website</a></li>
+  </ul>
+</nav>`}
+      />
+      <h2>Mega menu</h2>
+      <p>
+        The mega menu is placed in the bottom of the the header and is expanded upon hover of a menu item and is
+        included on all pages of www.kth.se.
+      </p>
+      <p>
+        This menu is also preferably imported to the application you are building via{' '}
+        <a href="https://github.com/KTH/kth-node-cortina-block" target="_blank" className="external-link">
+          kth-node-cortina-block
+        </a>{' '}
+        or similar functionality to keep the navigation in the header consistent throughout the sites.
+      </p>
     </div>
   )
 }
--- public/js/app/pages/__tests__/NavigationPage.test.js
+++ public/js/app/pages/__tests__/NavigationPage.test.js
@@ -22,7 +22,128 @@ test('Navigation page snapshot', () => {
           Navigation
         </h1>
         <p>
-          Navigation så som menyer ... 
+          Navigation is based on several menus in different contexts. Below you will find examples of these menus. 
+        </p>
+        <h2>
+          Main navigation
+        </h2>
+        <p>
+          The main navigation is placed on the left side of the main contnent. Real life examples can be found on KTH.se, for examle here: 
+          <a
+            href="https://www.kth.se/en/aktuellt/"
+          >
+            Aktuellt
+          </a>
+        </p>
+        <p>
+          A shortened example of the code can be seen below:
+        </p>
+        <div
+          class="code-snippet"
+        >
+          <div
+            class="code-snippet__container"
+          >
+            <pre>
+              <code
+                class="language-html"
+              >
+                &lt;nav id="mainMenu" class="col navbar navbar-expand-lg navbar-light"&gt;
+      &lt;div class="collapse navbar-collapse" id="navbarNav"&gt;
+          &lt;ul class="nav"&gt;
+            &lt;li class="parentLink"&gt;&lt;a href="/"&gt;KTH&lt;/a&gt;&lt;/li&gt;
+        &lt;/ul&gt;
+        &lt;ul class="nav nav-ancestor"&gt;
+          &lt;li&gt;&lt;span class="nav-item ancestor"&gt;Aktuellt&lt;/span&gt;&lt;/li&gt;
+          &lt;/ul&gt;
+          &lt;ul class="nav nav-list"&gt;
+            &lt;li class="nav-item leaf"&gt;&lt;a class="nav-link" href="/aktuellt/nyheter"&gt;Nyheter&lt;/a&gt;&lt;/li&gt;
+            &lt;li class="nav-item node"&gt;&lt;a class="nav-link" href="/aktuellt/nyhetsteman"&gt;Aktuella nyhetsteman&lt;/a&gt;&lt;/li&gt;
+            &lt;li class="nav-item node"&gt;&lt;a class="nav-link" href="/aktuellt/kalender"&gt;KTH-kalendern&lt;/a&gt;&lt;/li&gt;
+          &lt;/ul&gt;
+        &lt;/div&gt;
+      &lt;/nav&gt;
+              </code>
+            </pre>
+            <div
+              class="code-snippet__btn-container"
+            >
+              <button
+                class="btn"
+              >
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+        <h2>
+          Secondary menu
+        </h2>
+        <p>
+          The secondary menu is placed in the top right corner of the header and contains entrancies for students, alumnies, employees and a language switcher. This menu is more or less included on all pages of www.kth.se.
+        </p>
+        <p>
+          This menu is preferably imported to the application you are building via
+           
+          <a
+            class="external-link"
+            href="https://github.com/KTH/kth-node-cortina-block"
+            target="_blank"
+          >
+            kth-node-cortina-block
+          </a>
+           
+          or similar functionality to keep the navigation in the header consistent throughout the sites.
+        </p>
+        <div
+          class="code-snippet"
+        >
+          <div
+            class="code-snippet__container"
+          >
+            <pre>
+              <code
+                class="language-html"
+              >
+                &lt;nav aria-label="global secondary"&gt;
+      &lt;ul&gt;
+        &lt;li&gt;&lt;a href="/student"&gt;Student&lt;/a&gt;
+        &lt;/li&gt;&lt;li&gt;&lt;a href="/alumni"&gt;Alumn&lt;/a&gt;
+        &lt;/li&gt;&lt;li&gt;&lt;a href="https://intra.kth.se/"&gt;Anställd&lt;/a&gt;
+        &lt;/li&gt;&lt;li&gt;&lt;a href="https://www.kth.se/en" hreflang="en-UK"&gt;International website&lt;/a&gt;&lt;/li&gt;
+      &lt;/ul&gt;
+    &lt;/nav&gt;
+              </code>
+            </pre>
+            <div
+              class="code-snippet__btn-container"
+            >
+              <button
+                class="btn"
+              >
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+        <h2>
+          Mega menu
+        </h2>
+        <p>
+          The mega menu is placed in the bottom of the the header and is expanded upon hover of a menu item and is included on all pages of www.kth.se.
+        </p>
+        <p>
+          This menu is also preferably imported to the application you are building via
+           
+          <a
+            class="external-link"
+            href="https://github.com/KTH/kth-node-cortina-block"
+            target="_blank"
+          >
+            kth-node-cortina-block
+          </a>
+           
+          or similar functionality to keep the navigation in the header consistent throughout the sites.
         </p>
       </div>
     </div>
