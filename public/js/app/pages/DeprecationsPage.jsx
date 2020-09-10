500e8535458227533640ee1ffd3bf4806f512327
Niklas Olsson <hoyce@kth.se>
2020-09-10 13:58:33 +0200

    Removed and ordered icons. Also added deprecation info for icons

--- public/js/app/pages/DeprecationsPage.jsx
+++ public/js/app/pages/DeprecationsPage.jsx
@@ -18,13 +18,30 @@ export const Main = props => {
 
       <h2>Kommande major är 6.0.0</h2>
       <p>Nästa major kommer innehålla följade förändringar som innebär någon form av översyn eller åtgärd.</p>
-      <ul>
+      <ul className="deprecatedList">
         <li>
           Collapse kommer inte stödja uppbyggnad med button eller a-tag längre. Styling för dessa varianter utgår och
           ersätts av styling för details och summary istället. Exempel kan ses här:{' '}
           <a href="https://app-r.referens.sys.kth.se/style/collapse" alt="Exempel på Collapse-komponent">
             Collapse-exempel
           </a>
+        </li>
+        <li>
+          Följande ikoner utgår ur KTH Style:
+          <ul>
+            <li>back-link-pill.svg</li>
+            <li>back-link.svg</li>
+            <li>white-arrow.svg</li>
+            <li>clock_grey_25.svg</li>
+            <li>clock_grey_50.svg</li>
+            <li>clock_grey_100.svg</li>
+            <li>Clock_three_quarter.svg</li>
+            <li>canvas.svg</li>
+            <li>icon-from-canvas-blue.svg</li>
+            <li>pill-back-white.svg</li>
+            <li>pill-back.svg</li>
+            <li>select-arrow.png</li>
+          </ul>
         </li>
       </ul>
     </main>
--- public/js/app/pages/IconsPage.jsx
+++ public/js/app/pages/IconsPage.jsx
@@ -25,92 +25,92 @@ const ItemBox = props => {
   )
 }
 export const Main = props => {
-  const items = [
+  const alertItems = [
     { displayName: 'alert-attention.svg', url: 'icons/alert-attention.svg' },
     { displayName: 'alert-info.svg', url: 'icons/alert-info.svg' },
     { displayName: 'alert-success.svg', url: 'icons/alert-success.svg' },
+    { displayName: 'modal-info.svg', url: 'icons/modal-info.svg' },
+    { displayName: 'table-modal.svg', url: 'icons/table-modal.svg' },
+  ]
+  const arrowItems = [
     { displayName: 'arrow-black-down.svg', url: 'icons/arrow-black-down.svg' },
     { displayName: 'arrow-black-left.svg', url: 'icons/arrow-black-left.svg' },
     { displayName: 'arrow-black-right.svg', url: 'icons/arrow-black-right.svg' },
-    { displayName: 'arrow-cross-black.svg', url: 'icons/arrow-cross-black.svg' },
-    { displayName: 'arrow-gallery.svg', url: 'icons/arrow-gallery.svg' },
+    { displayName: 'arrow-white-down.svg', url: 'icons/arrow-white-down.svg', isWhite: true },
+    { displayName: 'arrow-white-left.svg', url: 'icons/arrow-white-left.svg', isWhite: true },
+    { displayName: 'arrow-white-right.svg', url: 'icons/arrow-white-right.svg', isWhite: true },
+    { displayName: 'arrow-blue-right.svg', url: 'icons/arrow-blue-right.svg' },
+    { displayName: 'arrow-blue-down.svg', url: 'icons/arrow-blue-down.svg' },
     { displayName: 'arrow-sort-asc-black.svg', url: 'icons/arrow-sort-asc-black.svg' },
     { displayName: 'arrow-sort-asc-white.svg', url: 'icons/arrow-sort-asc-white.svg', isWhite: true },
     { displayName: 'arrow-sort-default-black.svg', url: 'icons/arrow-sort-default-black.svg' },
     { displayName: 'arrow-sort-default-white.svg', url: 'icons/arrow-sort-default-white.svg', isWhite: true },
     { displayName: 'arrow-sort-desc-black.svg', url: 'icons/arrow-sort-desc-black.svg' },
     { displayName: 'arrow-sort-desc-white.svg', url: 'icons/arrow-sort-desc-white.svg', isWhite: true },
+    { displayName: 'arrow-cross-black.svg', url: 'icons/arrow-cross-black.svg' },
     { displayName: 'arrow-up-down-black.svg', url: 'icons/arrow-up-down-black.svg' },
     { displayName: 'arrow-up-down-white.svg', url: 'icons/arrow-up-down-white.svg', isWhite: true },
-    { displayName: 'arrow-white-down.svg', url: 'icons/arrow-white-down.svg', isWhite: true },
-    { displayName: 'arrow-white-left.svg', url: 'icons/arrow-white-left.svg', isWhite: true },
-    { displayName: 'arrow-white-right.svg', url: 'icons/arrow-white-right.svg', isWhite: true },
-    { displayName: 'back-link-pill.svg', url: 'icons/back-link-pill.svg' },
-    { displayName: 'back-link.svg', url: 'icons/back-link.svg' },
-    { displayName: 'balance-scale-black.svg', url: 'icons/balance-scale-black.svg' },
-    { displayName: 'balance-scale-white.svg', url: 'icons/balance-scale-white.svg', isWhite: true },
-    { displayName: 'canvas.svg', url: 'icons/canvas.svg' },
-    { displayName: 'check-mark.svg', url: 'icons/check-mark.svg' },
-    { displayName: 'clock_grey_25.svg', url: 'icons/clock_grey_25.svg' },
-    { displayName: 'clock_grey_50.svg', url: 'icons/clock_grey_50.svg' },
-    { displayName: 'clock_grey_100.svg', url: 'icons/clock_grey_100.svg' },
-    { displayName: 'clock_three_quarter.svg', url: 'icons/Clock_three_quarter.svg' },
+    { displayName: 'back-to-top.svg', url: 'back-to-top.svg' },
+  ]
+  const galleryItems = [
+    { displayName: 'arrow-gallery.svg', url: 'icons/arrow-gallery.svg' },
+    { displayName: 'pause-gallery.svg', url: 'icons/pause-gallery.svg' },
+    { displayName: 'play-gallery.svg', url: 'icons/play-gallery.svg' },
+  ]
+  const fileItems = [
     { displayName: 'document-white.svg', url: 'icons/document-white.svg', isWhite: true },
     { displayName: 'document.svg', url: 'icons/document.svg' },
-    { displayName: 'en_UK.svg', url: 'icons/en_UK.svg' },
-    { displayName: 'external-link.svg', url: 'icons/external-link.svg' },
     { displayName: 'file-archive-fa-regular.svg', url: 'icons/file-archive-fa-regular.svg' },
     { displayName: 'file-archive-fa-solid.svg', url: 'icons/file-archive-fa-solid.svg' },
     { displayName: 'file-audio-fa-regular.svg', url: 'icons/file-audio-fa-regular.svg' },
+    { displayName: 'file-video-fa-regular.svg', url: 'icons/file-video-fa-regular.svg' },
+    { displayName: 'file-video-fa-solid.svg', url: 'icons/file-video-fa-solid.svg' },
     { displayName: 'file-code-fa-regular.svg', url: 'icons/file-code-fa-regular.svg' },
     { displayName: 'file-code-fa-solid.svg', url: 'icons/file-code-fa-solid.svg' },
     { displayName: 'file-download-fa-solid.svg', url: 'icons/file-download-fa-solid.svg' },
+    { displayName: 'file-upload-fa-solid.svg', url: 'icons/file-upload-fa-solid.svg' },
+    { displayName: 'file-word-fa-regular.svg', url: 'icons/file-word-fa-regular.svg' },
+    { displayName: 'file-word-fa-solid.svg', url: 'icons/file-word-fa-solid.svg' },
     { displayName: 'file-excel-fa-regular.svg', url: 'icons/file-excel-fa-regular.svg' },
     { displayName: 'file-excel-fa-solid.svg', url: 'icons/file-excel-fa-solid.svg' },
     { displayName: 'file-export-fa-solid.svg', url: 'icons/file-export-fa-solid.svg' },
+    { displayName: 'file-import-fa-solid.svg', url: 'icons/file-import-fa-solid.svg' },
     { displayName: 'file-fa-regular.svg', url: 'icons/file-fa-regular.svg' },
     { displayName: 'file-fa-solid.svg', url: 'icons/file-fa-solid.svg' },
     { displayName: 'file-image-fa-regular.svg', url: 'icons/file-image-fa-regular.svg' },
     { displayName: 'file-image-fa-solid.svg', url: 'icons/file-image-fa-solid.svg' },
-    { displayName: 'file-import-fa-solid.svg', url: 'icons/file-import-fa-solid.svg' },
     { displayName: 'file-pdf-fa-regular.svg', url: 'icons/file-pdf-fa-regular.svg' },
     { displayName: 'file-pdf-fa-solid.svg', url: 'icons/file-pdf-fa-solid.svg' },
     { displayName: 'file-powerpoint-fa-regular.svg', url: 'icons/file-powerpoint-fa-regular.svg' },
     { displayName: 'file-powerpoint-fa-solid.svg', url: 'icons/file-powerpoint-fa-solid.svg' },
-    { displayName: 'file-upload-fa-solid.svg', url: 'icons/file-upload-fa-solid.svg' },
-    { displayName: 'file-video-fa-regular.svg', url: 'icons/file-video-fa-regular.svg' },
-    { displayName: 'file-video-fa-solid.svg', url: 'icons/file-video-fa-solid.svg' },
-    { displayName: 'file-word-fa-regular.svg', url: 'icons/file-word-fa-regular.svg' },
-    { displayName: 'file-word-fa-solid.svg', url: 'icons/file-word-fa-solid.svg' },
+  ]
+  const flagItems = [
+    { displayName: 'en_UK.svg', url: 'icons/en_UK.svg' },
+    { displayName: 'sv_SE.svg', url: 'icons/sv_SE.svg' },
     { displayName: 'globe-white.svg', url: 'icons/globe-white.svg', isWhite: true },
     { displayName: 'globe.svg', url: 'icons/globe.svg' },
-    { displayName: 'icon-from-canvas-blue.svg', url: 'icons/icon-from-canvas-blue.svg' },
+  ]
+  const otherItems = [
+    { displayName: 'balance-scale-black.svg', url: 'icons/balance-scale-black.svg' },
+    { displayName: 'balance-scale-white.svg', url: 'icons/balance-scale-white.svg', isWhite: true },
+    { displayName: 'check-mark.svg', url: 'icons/check-mark.svg' },
+    { displayName: 'external-link.svg', url: 'icons/external-link.svg' },
     { displayName: 'link-white.svg', url: 'icons/link-white.svg', isWhite: true },
-    { displayName: 'modal-info.svg', url: 'icons/modal-info.svg' },
-    { displayName: 'pause-gallery.svg', url: 'icons/pause-gallery.svg' },
     { displayName: 'pen-blue.svg', url: 'icons/pen-blue.svg' },
     { displayName: 'pen.svg', url: 'icons/pen.svg' },
     { displayName: 'picture-white.svg', url: 'icons/picture-white.svg', isWhite: true },
-    { displayName: 'pill-back-white.svg', url: 'icons/pill-back-white.svg', isWhite: true },
-    { displayName: 'pill-back.svg', url: 'icons/pill-back.svg' },
-    { displayName: 'play-gallery.svg', url: 'icons/play-gallery.svg' },
-    { displayName: 'sv_SE.svg', url: 'icons/sv_SE.svg' },
-    { displayName: 'table-modal.svg', url: 'icons/table-modal.svg' },
     { displayName: 'thumbs-down.svg', url: 'icons/thumbs-down.svg' },
     { displayName: 'thumbs-up.svg', url: 'icons/thumbs-up.svg' },
     { displayName: 'trash-can-blue.svg', url: 'icons/trash-can-blue.svg' },
     { displayName: 'trash-can-white.svg', url: 'icons/trash-can-white.svg', isWhite: true },
-    { displayName: 'white-arrow.svg', url: 'icons/white-arrow.svg', isWhite: true },
-    { displayName: 'back-to-top.svg', url: 'back-to-top.svg' },
     { displayName: 'input-clear-gray.svg', url: 'input-clear-gray.svg' },
     { displayName: 'input-clear.svg', url: 'input-clear.svg', isWhite: true },
     { displayName: 'KTH_Logotyp_RGB_2013-2.svg', url: 'KTH_Logotyp_RGB_2013-2.svg' },
     { displayName: 'quotation-mark.png', url: 'quotation-mark.png' },
     { displayName: 'search-icon.svg', url: 'search-icon.svg', isWhite: true },
-    { displayName: 'select-arrow.png', url: 'select-arrow.png' },
   ]
 
-  const itemsPane = items.map(item => (
+  const alertItemsPane = alertItems.map(item => (
     <ItemBox
       displayName={item.displayName}
       key={item.displayName}
@@ -120,10 +120,83 @@ export const Main = props => {
       width={item.width}
     />
   ))
+
+  const arrowItemsPane = arrowItems.map(item => (
+    <ItemBox
+      displayName={item.displayName}
+      key={item.displayName}
+      url={item.url}
+      isWhite={item.isWhite}
+      size={item.size}
+      width={item.width}
+    />
+  ))
+
+  const galleryItemsPane = galleryItems.map(item => (
+    <ItemBox
+      displayName={item.displayName}
+      key={item.displayName}
+      url={item.url}
+      isWhite={item.isWhite}
+      size={item.size}
+      width={item.width}
+    />
+  ))
+
+  const fileItemsPane = fileItems.map(item => (
+    <ItemBox
+      displayName={item.displayName}
+      key={item.displayName}
+      url={item.url}
+      isWhite={item.isWhite}
+      size={item.size}
+      width={item.width}
+    />
+  ))
+
+  const flagItemsPane = flagItems.map(item => (
+    <ItemBox
+      displayName={item.displayName}
+      key={item.displayName}
+      url={item.url}
+      isWhite={item.isWhite}
+      size={item.size}
+      width={item.width}
+    />
+  ))
+
+  const otherItemsPane = otherItems.map(item => (
+    <ItemBox
+      displayName={item.displayName}
+      key={item.displayName}
+      url={item.url}
+      isWhite={item.isWhite}
+      size={item.size}
+      width={item.width}
+    />
+  ))
+
   return (
     <main id="mainContent" className="mainContent">
       <h1>Ikoner</h1>
-      <div className="gridItemContainer">{itemsPane}</div>
+
+      <h2>Information</h2>
+      <div className="gridItemContainer">{alertItemsPane}</div>
+
+      <h2>Pilar</h2>
+      <div className="gridItemContainer">{arrowItemsPane}</div>
+
+      <h2>Galleri</h2>
+      <div className="gridItemContainer">{galleryItemsPane}</div>
+
+      <h2>Filer</h2>
+      <div className="gridItemContainer">{fileItemsPane}</div>
+
+      <h2>Flaggor</h2>
+      <div className="gridItemContainer">{flagItemsPane}</div>
+
+      <h2>Övriga</h2>
+      <div className="gridItemContainer">{otherItemsPane}</div>
     </main>
   )
 }
