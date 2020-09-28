60e10bb5c6baac3675d4d18bde953bdb3f1c6f9c
Niklas Olsson <hoyce@kth.se>
2020-09-28 14:39:12 +0200

    Fix build

--- .babelrc.js
+++ .babelrc.js
@@ -2,10 +2,6 @@ const isProd = process.env.NODE_ENV === 'production'
 const isTest = process.env.NODE_ENV === 'test'
 
 module.exports = {
-  presets: [['@babel/preset-env', { modules: isTest ? 'commonjs' : false }], '@babel/preset-react'],
-  plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-transform-runtime'],
-  ],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [['@babel/plugin-proposal-decorators', { legacy: true }], ['@babel/plugin-transform-runtime']],
 }
