16e0a02306e03e3fd9fd9244f3d7f1700ff3235a
Alexander Urban <lexa@toxa.de>
2020-10-28 16:13:03 +0100

    Basic setup revised: Dependencies, docker-local removed, nodemon, build.sh

--- build.sh
+++ build.sh
@@ -1,53 +1,128 @@
 #!/bin/bash
 
-# Colors
-yellow='\033[1;33m' # Yellow
-nc='\033[0m'        # No Color
+ENV=$1
+PROXY_PREFIX_PATH=$2
 
-echo -e "${yellow}"
-echo -e "|--------------------------------------------------------|"
-echo -e "| Building the application with Bash and Parcel          |"   
-echo -e "|--------------------------------------------------------|"
+[ "$BUILD_SH_LOG" == "debug" ] && ps -a
 
-echo -e "\n  1. Cleaning up & copying files"
+function main() {
+  echo
+  echoYellow "|--------------------------------------------------------|"
+  echoYellow "| Building the application with Bash and Parcel          |"   
+  echoYellow "|--------------------------------------------------------|\n"
 
-# Removing the dist folder
-echo -e "     -> Removing the dist folder"
-rm -rf ./dist
+  avoidZombieProcessesOnWindows
 
-# Creating the server views folder with sub folders systems and layouts
-echo -e "     -> Creating the server view folders"
-mkdir -p ./server/views/system ./server/views/layouts
+  if [ "$ENV" == "dev" ]; then
+    export NODE_ENV=development
+  else
+    export NODE_ENV=production
+  fi
 
-# Copy error.handlebars page to this project
-echo -e "     -> Copying error.handlebars to server/views/system folder"
-cp -R ./node_modules/kth-node-web-common/lib/handlebars/pages/views/. server/views/system
+  echoYellow "  1. Cleaning up & copying files"
 
-# Copy errorLayout.handlebars layout to this project
-echo -e "     -> Copying errorLayout.handlebars to server/views/layouts folder"
-cp -R ./node_modules/kth-node-web-common/lib/handlebars/pages/layouts/. server/views/layouts
+  # Removing the dist folder
+  if [ -d ./dist ]; then
+    echoYellow "     -> Removing all files from the /dist folder"
+    rm -rf ./dist/*
+  fi
 
-# Run parcel build on the vendor.js file and put the optimized file in the /public/js folder.
-echo -e "${yellow}\n  2. Parcing vendor.js and putting it in the /dist/js folder${nc}"
-npm run vendor
+  # Creating the server views folder with sub folders systems and layouts
+  echoYellow "     -> Creating the server view folders"
+  mkdir -p ./server/views/system ./server/views/layouts
 
-# Run parcel build on the /public/js files and put the optimized files in the /public/js folder.
-echo -e "${yellow}\n  3. Parcing the js views and putting them in the /dist folder${nc}"
-npm run parcel-views
+  # Copy error.handlebars page to this project
+  echoYellow "     -> Copying error.handlebars to server/views/system folder"
+  cp -R ./node_modules/kth-node-web-common/lib/handlebars/pages/views/. server/views/system
 
-if [ $1 == "prod" ]; then
-  # Run parcel on the scss files and put the optimized file in the /dist/css
-  echo -e "${yellow}\n  4. Transpiling sass files and putting them in the /dist folder${nc}"
-  npm run parcel-sass-prod
-fi
+  # Copy errorLayout.handlebars layout to this project
+  echoYellow "     -> Copying errorLayout.handlebars to server/views/layouts folder"
+  cp -R ./node_modules/kth-node-web-common/lib/handlebars/pages/layouts/. server/views/layouts
 
-# Only run Parcel watch in development
-if [ $1 == "dev" ]; then
-# Run parcel on the scss files and put the optimized file in the /dist/css
-  echo -e "${yellow}\n  4. Transpiling sass files and putting them in the /dist folder${nc}"
-  npm run parcel-sass-dev
+  # Run parcel build on the vendor.js file and put the optimized file into the /dist folder.
+  echo
+  echoYellow "  2. Bundling vendor.js into the /dist folder\n"
+  if [ "$AVOID_ZOMBIE_PROCESSES" == "true" ]; then
+    parcel build ./public/js/vendor.js --public-url $PROXY_PREFIX_PATH/static &
+    memorizePidAndWaitForProcessToFinish $!
+  else
+    parcel build ./public/js/vendor.js --public-url $PROXY_PREFIX_PATH/static
+  fi
 
-  # Run parcel watch on the js and sass files and put the optimized files in the /dist folder upon change.
-  echo -e "${yellow}\n  5. Running watch on js views and sass files${nc}"
-  npm run parcel-watch
-fi
+  if [ "$ENV" == "prod" ]; then
+    # Run parcel build on the files in /public/js/app and put the optimized files into the /dist folder.
+    echo
+    echoYellow "  3. Bundling the client app into the /dist folder\n"
+    if [ "$AVOID_ZOMBIE_PROCESSES" == "true" ]; then
+      parcel build './public/js/app/{*.js,*.jsx}' --public-url $PROXY_PREFIX_PATH/static &
+      memorizePidAndWaitForProcessToFinish $!
+    else
+      parcel build './public/js/app/{*.js,*.jsx}' --public-url $PROXY_PREFIX_PATH/static
+    fi
+
+    echo
+    echoYellow "  Done.\n"
+  fi
+
+  # Only run Parcel watch in development
+  if [ "$ENV" == "dev" ]; then
+    # Run parcel build on the files in /public/js/app and put the optimized files into the /dist folder.
+    echo
+    echoYellow "  3. Bundling the client app into the /dist folder to list results\n"
+    if [ "$AVOID_ZOMBIE_PROCESSES" == "true" ]; then
+      parcel build --no-minify './public/js/app/{*.js,*.jsx}' --public-url http://localhost:3000$PROXY_PREFIX_PATH/static &
+      memorizePidAndWaitForProcessToFinish $!
+    else
+      parcel build --no-minify './public/js/app/{*.js,*.jsx}' --public-url http://localhost:3000$PROXY_PREFIX_PATH/static
+    fi
+
+    # Run parcel watch on the files in /public/js/app and put the optimized files into the /dist folder.
+    echo
+    echoYellow "  4. Running watch on client app. Check /dist for changes\n"
+    if [ "$AVOID_ZOMBIE_PROCESSES" == "true" ]; then
+      parcel watch './public/js/app/{*.js,*.jsx}' --public-url http://localhost:3000$PROXY_PREFIX_PATH/static &
+      memorizePidAndWaitForProcessToFinish $!
+    else
+      parcel watch './public/js/app/{*.js,*.jsx}' --public-url http://localhost:3000$PROXY_PREFIX_PATH/static
+    fi
+  fi
+}
+
+function avoidZombieProcessesOnWindows() {
+  SYSTEM=`uname`
+  if [ "${SYSTEM:0:7}" == "MINGW64" ]; then
+    [ "$BUILD_SH_LOG" == "debug" ] && echo -e "  (Windows Git Bash detected)\n"
+    AVOID_ZOMBIE_PROCESSES=true
+    LATEST_PID=
+
+    trap avoidZombieProcessAfterControlC SIGINT
+  fi
+}
+
+function memorizePidAndWaitForProcessToFinish() {
+  LATEST_PID=$1
+  [ "$BUILD_SH_LOG" == "debug" ] && echo -e "  (Waiting...)\n"
+  wait $LATEST_PID
+  [ "$BUILD_SH_LOG" == "debug" ] && echo -e "\n  (Ready.)"
+  LATEST_PID=
+}
+
+function avoidZombieProcessAfterControlC() {
+  [ "$BUILD_SH_LOG" == "debug" ] && ps -a
+  if [ -n "$LATEST_PID" ]; then
+    echo -e "\n\n  (Ensuring that process with PID $LATEST_PID is gone...)"
+    if kill -0 $LATEST_PID > /dev/null 2>&1; then
+      [ "$BUILD_SH_LOG" == "debug" ] && echo -e "  (Killing process with PID $LATEST_PID)"
+      kill $LATEST_PID
+    fi
+  fi
+  [ "$BUILD_SH_LOG" == "debug" ] && echo "  (Cleanup done.)"
+  exit 0
+}
+
+function echoYellow() {
+  MSG=$1
+  printf "\033[1;33m$MSG\033[0m\n"
+}
+
+main
